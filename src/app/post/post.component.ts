535917d8c9ed17fcb88f4f77eb3c12bae94b624d
Parmedio <marco.da.pieve@gmail.com>
2023-08-11 14:49:55 +0200

    alias nad passing info to components

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -6,6 +6,8 @@ import { Component, Input, EventEmitter } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  @Input() postImg = '';
+  @Input('bubba') postImg = ''; 
   imgSelected = new EventEmitter<string>();
 }
+
+//se non smettevo nulla dentro al posto di bubba nel componente padre avrei dovuto usare 'postImg' nel foglio html
