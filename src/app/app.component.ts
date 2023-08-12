71cf364ed723161f1815a869ff0d6be6a312c95d
Parmedio <marco.da.pieve@gmail.com>
2023-08-12 17:28:50 +0200

    date pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'basics knowledge';
   doggo = 'https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
   imageFullURL = '';
+  currentDate = new Date();
   getName = () => {
     return "Crema solare";
   }
